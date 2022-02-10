0b7efdbe18d56e9b609b0fefd96dd50b04ab3937
luiscapello <capello3005@gmail.com>
2022-02-10 15:54:29 -0600

    servicios

--- AppSalon_fin/include/funciones.php
+++ AppSalon_fin/include/funciones.php
@@ -1,19 +1,37 @@
 <?php
 
-function obtenerServicios() {
+function obtenerServicios() : array{
     try{
         
         //importar conexion
         require 'database.php';
 
-        echo '$db';
-        var_dump($db);
+        /*echo '$db';
+        var_dump($db);*/
 
         //escribir el codigo sql
+        $sql = "SELECT * FROM servicios;";
 
+        $consulta = mysqli_query($db, $sql);
+
+        //Arreglo vacio
+        $servicios = [];
+
+        $i = 0;
 
         //obtener los resultados
+        while( $row = mysqli_fetch_assoc($consulta)){
+            $servicios[$i]['id'] = $row['id'];
+            $servicios[$i]['nombre'] = $row['nombre'];
+            $servicios[$i]['precio'] = $row['precio'];
 
+            $i++;
+        }
+        // echo"<pre>";
+        // var_dump($servicios);
+        // echo"</pre>";
+        
+        return $servicios;
 
 
     }catch (\Throwable $th) {
@@ -22,3 +40,5 @@ function obtenerServicios() {
         var_dump($th);
     }
 }
+
+obtenerServicios();
