25440cc7a43575c1201e0610d724ef8fd3529748
luiscapello <capello3005@gmail.com>
2022-02-10 17:37:24 -0600

    Update index.php

--- crud_php/index.php
+++ crud_php/index.php
@@ -20,7 +20,78 @@
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+    <div class="container">
+        <h1 class="text-center">Luis Capello</h1>
+
+        <h1 class="text-center">Uyeda industrial de Mexico</h1>
+
+        <div class="row">
+            <div class="clo-2 offset-6">
+                <div class="text-center">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary w-90" data-bs-toggle="modal" data-bs-target="#modalUsuario" id="botonCrear">
+                    <i class="bi bi-plus-circle-fill"></i> Crear
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <br/>
+        <br/>
+        <br/>
+
+        <div class="tableresponsive">
+            <table id="datos-usuario" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Email</th>
+                        <th>Fecha creación</th>
+                        <th>Editar</th>
+                        <th>Borrar</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+
+    <!--Modal-->
+    <div class="modal fade" id="modalUsuario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Cotizaciones</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <form method="POST" id="formulario" enctype="multipart/form-data">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <label for="nombre">Ingrese el Nombre</label>
+                            <input type="text" name="nombre" id="nombre" class="form-control">
+                            <br/>
+
+                            <label for="apellido">Ingrese el Apellido</label>
+                            <input type="text" name="apellido" id="apellido" class="form-control">
+                            <br/>
+
+                            <label for="email">Ingrese el Email</label>
+                            <input type="email" name="email" id="email" class="form-control">
+                            <br/>
+                        </div>
+                        <div class="modal-footer">
+                            <input type="hidden" name="id_usuario" id="id_usuario">
+                            <input type="hiddden" name="operacion" id="operacion">
+                            <input type="submit" name="action" id="action" class="btn btn-success" value="crear">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
